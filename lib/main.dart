1dfc47f6deb9ea8e5eaa446cefb7c71247b71d52
Sebastian Ibarra <andresib038@gmail.com>
2024-09-25 18:12:12 -0700

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCoKQfgmYz93-vyLP64g0m9fP3jGsDwkyw",
+      projectId: "suitespot-8d1ba",
+      messagingSenderId: "405319714500",
+      appId: "1:405319714500:web:af37448d80d1790e4deb91",
+    )
+  );
   runApp(const MainApp());
 }
 
